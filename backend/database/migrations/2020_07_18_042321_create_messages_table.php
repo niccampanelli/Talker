a3c721e8c3c977b94b7fc3343ba1d4378e0b3d13
Nicholas Campanelli <nicholasoucampanelli@hotmail.com>
2020-07-18 01:42:22 -0300

    Making Migrations

--- backend/database/migrations/2020_07_18_042321_create_messages_table.php
+++ backend/database/migrations/2020_07_18_042321_create_messages_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateMessagesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('messages', function (Blueprint $table) {
             $table->increments("id");
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string("content");
+            $table->integer("user_id")->unsigned();
+            $table->foreign("user_id")->references("id")->on("users");
+            $table->integer("receptor_id")->unsigned();
+            $table->foreign("receptor_id")->references("id")->on("users");
             $table->timestamps();
         });
     }
@@ -31,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('messages');
     }
 }
---
+++ backend/database/migrations/2020_07_18_042946_create_contacts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContactsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->increments("id");
+            $table->string("contact_name");
+            $table->integer("user_id")->unsigned();
+            $table->foreign("user_id")->references("id")->on("users");
+            $table->integer("contact_id")->unsigned();
+            $table->foreign("contact_id")->references("id")->on("users");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contacts');
+    }
+}
